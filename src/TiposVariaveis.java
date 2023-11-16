f19d6ef4d93c679473758e34b8603c246e820b6e
Dev-JeanCharles <jeancduasan@gmail.com>
2023-11-16 18:04:46 -0300

    Tipos de variaveis e constantes

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        byte	| 1 byte  |
+//        short	| 2 byte  |
+//        int	| 4 bytes |
+//        long	| 8 bytes |
+
+//        float	 | 4 bytes |
+//        double | 8 bytes |
+
+//      Conversão de tipos de dados primitivos
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+//      Casting!
+        short numeroCurto2 = (short) numeroNormal;
+
+
+//      Constantes
+        final int NUMERO_UM = 1;
+
+        System.out.print(NUMERO_UM);
+
     }
+
 }
